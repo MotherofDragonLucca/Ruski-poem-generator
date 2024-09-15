d7b80eaa5d25a0749d5231f513ed8fb077cf84cd
Farida Scandrett <fom111211@gmail.com>
2024-09-15 23:36:29 +1000

    Improved User Experience

--- src/index.js
+++ src/index.js
@@ -13,15 +13,14 @@ function displayPoem(response) {
 function generatePoem(event) {
   event.preventDefault();
   let instructionsInput = document.querySelector("#user-instructions");
+  const poemElement = document.querySelector("#poem"); // Define poemElement here
 
   let apiKey = "2cdb3f5e83a43fbc4a0edc02t2ef5ofc";
   let context =
-    "You are a Russian Poem expert and love to write short romantic poems in Russian. Your mission is to generate a 4 line poem in basic HTML and separate each line with a <br />. Then add a a <br /> and provide translation to English and separate each line with a <br />. Make sure to follow the user instructions. Do not include a title to the poem. Sign the poem with 'SheCodes AI' inside a <strong> element at the end of the poem after translation only and NOT at the beginning. Add <br /> before signing.";
+    "You are a Russian Poem expert and love to write short romantic poems in Russian. Your mission is to generate a 4 line poem in basic HTML and separate each line with a <br />. Then add a <br /> and provide translation to English and separate each line with a <br />. Make sure to follow the user instructions. Do not include a title to the poem. Sign the poem with 'SheCodes AI' inside a <strong> element at the end of the poem after translation only and NOT at the beginning. Add <br /> before signing.";
   let prompt = `User instructions: Generate a Russian poem about ${instructionsInput.value}`;
   let apiURL = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
-  console.log("Generating poem");
-  console.log(`Prompt: ${prompt}`);
-  console.log(`Context: ${context}`);
+  poemElement.innerHTML = `<div class="generating">⏳ Generating a Russian poem about ${instructionsInput.value}</div>`;
 
   axios.get(apiURL).then(displayPoem);
 }
